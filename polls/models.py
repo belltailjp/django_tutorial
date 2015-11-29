1a92acf03e77ea5428ad1d268c9f7fe877bebb3a
belltailjp <belltailjp@gmail.com>
2015-11-29 18:49:35 +0900

    Implemented conversion functions

--- polls/models.py
+++ polls/models.py
@@ -4,8 +4,14 @@ class Question(models.Model):
     question_text = models.CharField(max_length = 256)
     pub_date = models.DateTimeField('date published')
 
+    def __str__(self):
+        return "%s (%s)" % (self.question_text, self.pub_date)
+
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     choice_text = models.CharField(max_length = 256)
     votes = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return "%d: %s (vote: %d)" % (self.question.id, self.choice_text, self.votes)
 
