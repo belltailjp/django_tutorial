93b8c267054164886f7461fa4b517c332ea6ee46
belltailjp <belltailjp@gmail.com>
2015-11-29 22:32:41 +0900

    Implemented some actions

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from polls.models import *
 
-# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')
+    template = loader.get_template('polls/index.html')
+    context = RequestContext(request, {
+        'latest_question_list': latest_question_list,
+    })
+    return HttpResponse(template.render(context))
+
+def detail(request, question_id):
+    return HttpResponse('Detail of %s' % Question.objects.get(id = question_id))
+
+def results(request, question_id):
+    return HttpResponse('Results of %s' % Question.objects.get(id = question_id))
+
+def vote(request, question_id):
+    return HttpResponse('Votes of %s' % Question.objects.get(id = question_id))
