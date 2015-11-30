902b35ae1e026b050387661bb45694c8f1be3d92
belltailjp <belltailjp@gmail.com>
2015-11-30 18:31:15 +0900

    Refactored index action

--- polls/views.py
+++ polls/views.py
@@ -4,12 +4,10 @@ from django.template import RequestContext, loader
 from polls.models import *
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')
-    template = loader.get_template('polls/index.html')
-    context = RequestContext(request, {
-        'latest_question_list': latest_question_list,
-    })
-    return HttpResponse(template.render(context))
+    context = {
+        'latest_question_list': Question.objects.order_by('-pub_date'),
+    }
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
     return HttpResponse('Detail of %s' % Question.objects.get(id = question_id))
