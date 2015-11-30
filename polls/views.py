4c97f2454b4a14338b94b9f521165c7d1958e3ea
belltailjp <belltailjp@gmail.com>
2015-11-30 19:02:50 +0900

    Refactored Question#detail action

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 from django.template import RequestContext, loader
 from polls.models import *
@@ -10,13 +10,9 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(id = question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-
-    context = {'question': question}
-    return render(request, 'polls/detail.html', context)
+    return render(request, 'polls/detail.html', {
+            'question' : get_object_or_404(Question, id = question_id)
+        })
 
 def results(request, question_id):
     return HttpResponse('Results of %s' % Question.objects.get(id = question_id))
