2db3f7039250c8bc00853bca22c5d64ed6c3ba86
belltailjp <belltailjp@gmail.com>
2015-12-01 20:14:34 +0900

    Implemented POST vote function

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import RequestContext, loader
+from django.core.urlresolvers import reverse
 from polls.models import *
 
 def index(request):
@@ -18,4 +19,15 @@ def results(request, question_id):
     return HttpResponse('Results of %s' % Question.objects.get(id = question_id))
 
 def vote(request, question_id):
-    return HttpResponse('Votes of %s' % Question.objects.get(id = question_id))
+    p = get_object_or_404(Question, id = question_id)
+    try:
+        selected_choice = p.choice_set.get(id = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question' : p,
+            'error_message' : 'You didn\' select a correct choice',
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args = (p.id,)))
