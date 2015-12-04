71cd415094c2d3173f7290bcdb68036892cf0dbc
belltailjp <belltailjp@gmail.com>
2015-12-04 20:49:12 +0900

    Write more model test

--- polls/tests.py
+++ polls/tests.py
@@ -7,5 +7,13 @@ from polls.models import *
 class QuestionTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         q = Question(question_text = 'future', pub_date = timezone.now() + datetime.timedelta(days = 30))
-        self.assertEqual(q.was_published_recently(), False)
+        self.assertFalse(q.was_published_recently())
+
+    def test_was_published_recently_with_future_old_question(self):
+        q = Question(question_text = 'future', pub_date = timezone.now() - datetime.timedelta(days = 30))
+        self.assertFalse(q.was_published_recently())
+
+    def test_was_published_recently_with_recent_question(self):
+        q = Question(question_text = 'future', pub_date = timezone.now() - datetime.timedelta(days = 1) + datetime.timedelta(seconds = 1))
+        self.assertTrue(q.was_published_recently())
 
