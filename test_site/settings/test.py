37a5f2eb0156b586ccc5cfe94357d62889d8a163
belltailjp <belltailjp@gmail.com>
2015-12-04 21:54:49 +0900

    Created dev and test environment

---
+++ test_site/settings/test.py
@@ -0,0 +1,14 @@
+"""
+Configuration for local development environment
+"""
+
+from test_site.settings.base import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
+    }
+}
